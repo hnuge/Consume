17f69f17d3af6031cbe2981e43ecb1287a99cada
H. Nugent <nugent_h2@denison.edu>
2017-04-12 21:48:03 -0400

    initial commit of Consume

---
+++ Consume2 4:6/Assets/Scripts/squre.js
@@ -0,0 +1,67 @@
+﻿#pragma strict
+
+var square:GameObject;
+var lastKey:String;
+var currKey:String;
+
+var speed : float = 0.25;
+
+var array = new Array();
+
+
+
+function Start () {
+	currKey = "start";
+}
+
+function Update () {
+	/*-----------------------------------------
+		Movement 
+		Square moves exactly one length of itself
+	-----------------------------------------*/
+	if (Input.GetKey("up"))
+	{
+		lastKey = "up";
+		transform.position.y +=.5 * speed;
+	}
+	else if (Input.GetKey("down"))
+	{
+		lastKey = "down";
+		transform.position.y -=.5 * speed;
+	}
+	else if (Input.GetKey("left"))
+	{
+		lastKey = "left";
+		transform.position.x -=.5 * speed;
+	}
+	else if (Input.GetKey("right"))
+	{
+		lastKey = "right";
+		transform.position.x +=.5 * speed;
+	}
+
+	if (lastKey != currKey)
+	{
+		array.Push(transform.position);
+		print(array);
+	}
+
+	currKey = lastKey;
+}
+
+
+function OnTriggerEnter2D(hit : Collider2D)
+{
+	if (hit.gameObject.tag == "home")
+	{
+		print(array);
+		array.length = 0; 	// clearing array
+		print(array);
+	}
+
+}
+
+function turnCorners()
+{
+	
+}
---
+++ Consume2 4:6/Assets/Scripts/title.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+//consume
+
+function Start () {
+}
+
+function OnGUI(){
+	GUILayout.BeginArea (Rect(0,0,Screen.width, Screen.height));
+	StartPrompt();
+
+	GUILayout.EndArea();
+}
+
+function StartPrompt () {
+	var winPromptW:int = 400;
+	var winPromptH:int = 400;
+	var halfScreenW:float = Screen.width/2;
+	var halfScreenH:float = Screen.height/2;
+	var halfPromptW:int = winPromptW/2;
+	var halfPromptH:int = winPromptH/2;
+
+	GUI.BeginGroup(Rect(halfScreenW-halfPromptW, halfScreenH-halfPromptH, winPromptW, winPromptH));
+	//GUI.Box(Rect(0,0,winPromptW, winPromptH), "Click below to play.");
+	var buttonW:int = 100;
+	var buttonH:int = 20;
+
+	if (GUI.Button(Rect(halfPromptW-(buttonW/2), (halfPromptH-(buttonH/2))/.7, buttonW, buttonH), "Start Game"))
+	{
+		Application.LoadLevel("level1");
+	}
+	GUI.EndGroup();
+}
