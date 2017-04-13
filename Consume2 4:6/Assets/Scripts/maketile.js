893e71d53fdbc51b9d35a788d66672c80642f591
H. Nugent <nugent_h2@denison.edu>
2017-04-13 15:24:02 -0400

    initial grid

--- Consume2 4:6/Assets/Scripts/maketile.js
+++ Consume2 4:6/Assets/Scripts/maketile.js
@@ -1,7 +1,28 @@
 ﻿#pragma strict
 
+var prefab : GameObject;
+var cells : GameObject[,];
+
+public var rows : int;
+public var columns : int;
+
+
+
 function Start () {
-	
+	cells = new GameObject[rows, columns];
+
+	var i : int;
+	var j : int;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < columns; j++)
+		{
+			cells[i,j] = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+			cells[i,j].GetComponent.<Renderer>().material.color = Color.red;
+
+		}
+
+	}
 }
 
 function Update () {
