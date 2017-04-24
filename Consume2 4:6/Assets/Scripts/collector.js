7641c59cacd0012e951e704be8f78039b98b46d5
shalerc1 <jimannshaler@Cole-Shalers-MacBook-6.local>
2017-04-23 22:42:48 -0400

    Collector chase
    The collector AI targets and moves towards coins properly.

--- Consume2 4:6/Assets/Scripts/collector.js
+++ Consume2 4:6/Assets/Scripts/collector.js
@@ -14,26 +14,33 @@ function Update () {
 		var xCo = 1;
 		var yCo = 1;
 		var coins = GameObject.FindGameObjectsWithTag("coin");
-		var target = Number.PositiveInfinity;
-		for (c = 0; c < coins.length; c++){
-			
+		var dist = Number.PositiveInfinity;
+		for (var c = 0; c < coins.length; c++){
+			var q = (coins[c].transform.position.x-transform.position.x)*(coins[c].transform.position.x-transform.position.x)+(coins[c].transform.position.y-transform.position.y)*(coins[c].transform.position.y-transform.position.y);
+			if (q < dist)
+			{
+				dist = q;
+				var target = coins[c];
+			}
+		var coinX = target.transform.position.x;
+		var coinY = target.transform.position.y;
 		}
-//		if (transform.position.x >squareX)
-//		{
-//			xCo = -1;
-//		}
-//		if (transform.position.y >squareY)
-//		{
-//			yCo = -1;
-//		}
-//		if (Mathf.Abs(squareX-transform.position.x) > Mathf.Abs(squareY-transform.position.y))
-//		{
-//			transform.position.x += xCo;
-//		}
-//		else
-//		{
-//			transform.position.y += yCo;
-//		}
-//		count = 0;
+		if (transform.position.x >coinX)
+		{
+			xCo = -1;
+		}
+		if (transform.position.y >coinY)
+		{
+			yCo = -1;
+		}
+		if (Mathf.Abs(coinX-transform.position.x) > Mathf.Abs(coinY-transform.position.y))
+		{
+			transform.position.x += xCo;
+		}
+		else
+		{
+			transform.position.y += yCo;
+		}
+		count = 0;
 	}
 }
