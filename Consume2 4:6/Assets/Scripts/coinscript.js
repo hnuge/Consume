ce875300e777e177a6d9de1f9521d702126a0d58
H. Nugent <nugent_h2@denison.edu>
2017-04-23 22:13:12 -0400

    coin script

---
+++ Consume2 4:6/Assets/Scripts/coinscript.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var coin : GameObject;
+var numcoins : int;
+var c : GameObject;
+
+var coinloc : int[,];
+
+public var ts : tilescript;  	//access maketile script
+
+function Start () 
+{
+	c = new GameObject[columns * rows];
+	var x : int;
+	var y : int;
+	var i : int;
+	for (i = 0; i < numcoins; i++)
+	{
+		x = Random.Range(0, ts.columns);
+		y = Random.Range(0, ts.rows);
+		c[i] = Instantiate(coin, new Vector3(x, y, 0), Quaternion.identity);
+		coinloc[x, y] = 1;
+
+
+	}
+
+}
+
+function Update () 
+{
+	
+}
